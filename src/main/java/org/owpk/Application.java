01b6497c63278324149d02cefc60e0de1281313f
owpk <vzvz4@mail.ru>
2021-07-02 23:44:10 +0800

    chart option update

--- src/main/java/org/owpk/Application.java
+++ src/main/java/org/owpk/Application.java
@@ -6,8 +6,17 @@ import picocli.CommandLine;
 public class Application implements Runnable {
     private View<String> view;
 
-    @CommandLine.Option(names = {"-p", "--pair"})
+    @CommandLine.Option(names = {"-p", "--pair"}, description = "crypto pair", defaultValue = "BTC-USD")
     private String currencyPair;
+
+    @CommandLine.Option(names = {"-d", "--delay"}, description = "timeframe delay", defaultValue = "500")
+    private int delay;
+
+    @CommandLine.Option(names = {"-x", "--x-axis"}, description = "x axis length", defaultValue = "60")
+    private int x;
+
+    @CommandLine.Option(names = {"-y", "--y-axis"}, description = "y axis length", defaultValue = "15")
+    private int y;
 
     public static void main(String[] args) {
         new CommandLine(Application.class).execute(args);
@@ -15,17 +24,15 @@ public class Application implements Runnable {
 
     @Override
     public void run() {
-        view = new CliView();
-        if (currencyPair != null) {
-            view.getEventHandler().setMessageProvider(
-                    new MessageProvider(){
-                @Override
-                public String getMessage() {
-                    return "{\"type\": \"subscribe\",\"product_ids\": [\"" + currencyPair
-                            + "\"],\"channels\": [\"full\"]}";
-                }
-            });
-        }
+        var msgProvider = new MessageProvider() {
+            @Override
+            public String getMessage() {
+                return "{\"type\": \"subscribe\",\"product_ids\": [\"" + currencyPair
+                        + "\"],\"channels\": [\"full\"]}";
+            }
+        };
+        view = new CliView(x, y, delay);
+        view.getEventHandler().setMessageProvider(msgProvider);
         var wsCtx = new WebSocketClientContext("wss://ws-feed.pro.coinbase.com", view);
         wsCtx.connect();
     }
--- src/main/java/org/owpk/CliView.java
+++ src/main/java/org/owpk/CliView.java
@@ -2,21 +2,35 @@ package org.owpk;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mitchtalmadge.asciidata.graph.ASCIIGraph;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+@Getter
+@Setter
 public class CliView implements View<String> {
-    private static final Integer Y_AXIS = 60;
-    private static final Integer X_AXIS = 15;
+    private static final Integer X_AXIS = 60;
+    private static final Integer Y_AXIS = 15;
     private static final Integer TIMER_DELAY = 500;
     private final ObjectMapper objectMapper;
     private final Queue<Double> chartData;
     private EventSocket eventSocket;
+    private Integer yAxis;
+    private Integer xAxis;
+    private Integer timerDelay;
     private int timer;
 
     public CliView() {
+        this(X_AXIS, Y_AXIS, TIMER_DELAY);
+    }
+
+    public CliView(Integer x, Integer y, Integer timerDelay) {
+        this.yAxis = y;
+        this.xAxis = x;
+        this.timerDelay = timerDelay;
         this.eventSocket = new EventSocket(this, new MessageProvider());
         this.objectMapper = new ObjectMapper();
         this.chartData = new ArrayDeque<>();
@@ -34,7 +48,7 @@ public class CliView implements View<String> {
 
     @Override
     public void handleData(String data) {
-        if (timer == 0 || timer > TIMER_DELAY) {
+        if (timer == 0 || timer > timerDelay) {
             try {
                 if (timer > 0)
                     clear();
@@ -51,15 +65,15 @@ public class CliView implements View<String> {
     private void render(String data) throws IOException {
         var response = (CoinBaseResponse) objectMapper.readValue(data, CoinBaseResponse.class);
         double val = Double.parseDouble(response.getPrice());
-        if (chartData.size() > Y_AXIS)
+        if (chartData.size() > xAxis)
             chartData.poll();
         chartData.add(val);
         double[] arr = chartData.stream().mapToDouble(Double::doubleValue).toArray();
-        System.out.println(ASCIIGraph.fromSeries(arr).withNumRows(X_AXIS).plot());
+        System.out.println(ASCIIGraph.fromSeries(arr).withNumRows(yAxis).plot());
     }
 
     private void clear() {
-        for (int i = 0; i < X_AXIS + 1; i++) {
+        for (int i = 0; i < yAxis + 1; i++) {
             System.out.printf("\033[%dA", 1);
             System.out.print("\033[2K");
         }
